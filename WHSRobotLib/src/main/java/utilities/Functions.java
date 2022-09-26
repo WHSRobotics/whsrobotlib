cfd325acb6c74d38ecd28d3bb1c417658714a6f1
perryhan0123@gmail.com <perryhan0123@gmail.com>
2022-09-25 20:38:33 -0700

    Added WHSRobotLib Module

--- WHSRobotLib/src/main/java/utilities/Functions.java
+++ WHSRobotLib/src/main/java/utilities/Functions.java
@@ -25,7 +25,7 @@ public class Functions {
      * @return The new scaled value
      * @see #map(double, double, double, double, double)
      */
-    public double map(double input, double min, double max){
+    public static double map(double input, double min, double max){
         if(input < min) return min;
         if(input > max) return max;
         return (input-min)*(max-min)/(max-min) + min;
@@ -41,7 +41,7 @@ public class Functions {
      * @return
      * @see #map(double, double, double)
      */
-    public double map(double input, double in_min, double in_max, double out_min, double out_max){
+    public static double map(double input, double in_min, double in_max, double out_min, double out_max){
         if(in_min >= in_max) throw new IllegalArgumentException("in_min cannot be greater than in_max");
         if(out_min >= out_max) throw new IllegalArgumentException("out_min cannot be greater than out_max");
         if(input <= in_min) return out_min;
